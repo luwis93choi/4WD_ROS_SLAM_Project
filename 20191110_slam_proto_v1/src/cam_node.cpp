b48380752b623ab1648117d4149054b57cd516ae
luwis93choi <luwis93choi@hotmail.com>
2019-11-10 15:42:58 +0900

    Editted cam_node.cpp of slam proto v1

--- 20191110_slam_proto_v1/src/cam_node.cpp
+++ 20191110_slam_proto_v1/src/cam_node.cpp
@@ -13,13 +13,18 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 
-int image_RX_count;
+int image_RX_count; // Number of received images
 
+// This callback function will be invoked if image subscriber receives images.
+// This function receives images as sensor messages
 void imageCallback(const sensor_msgs::ImageConstPtr& msg){
 
-    cv_bridge::CvImagePtr cv_ptr;
+    // In order to utilize OpenCV, we need to image data that is compatible with OpenCV standards.
+    // ROS provides cv_bridge as a mean to convert image sensor message into OpenCV-compatible variable.
 
-    image_RX_count++;
+    cv_bridge::CvImagePtr cv_ptr;   // Image data pointer : cv_bridge turns image sensor message into image object pointer with various info and attributes
+
+    image_RX_count++;   // Increase the number if image is received
 
     ROS_INFO("imageCallback call count : %d", image_RX_count);
     ROS_INFO("[Image height : %d] [Image width : %d] [Image encoding : %s] \n", msg->height, msg->widht, msg->ending());
@@ -27,8 +32,11 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg){
     // Use 'cv_bridge' and its 'toCvCopy' in order to convert ROS msgs Image into OpenCV Image
     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
 
-    cv::imshow("USB_CAM image", cv_ptr->image); // Actual image data is stored in member variable 'image' of CvImagePtr
-    cv::waitKey(3);
+    cv::imshow("USB_CAM image", cv_ptr->image);     // Display camera image on the window with title "USB_CAM image"
+    // Actual image data is stored in member variable 'image' of CvImagePtr
+    // To look up on details of CvImagePtr, refer to ROS sensor_msgs::Image documenation
+
+    cv::waitKey(3); // Maintain the image window until any IO inputs
 }
 
 int main(int argc, char** argv){
@@ -51,17 +59,20 @@ int main(int argc, char** argv){
     // As a result, in order for the node to receive the image data, it has to subscribe the image just as it does for receiving standard messages from other nodes.
     image_transport::Subscriber sub = it.subscribe("/usb_cam/image_raw", 1000, imageCallback);
 
-    
+    // When image subscriber receives images, it will invoke imageCallback function defined above.
+
+    // Use spinOnce in order to handle all the message Callbacks and return them immediately
     ros::spinOnce();
 
+    // While roscore is operational, loop will continue on...
     while(ros::ok()){
 
-        ros::spinOnce();
+        ros::spinOnce();    // Handle all the message Callbacks
 
-        loop_rate.sleep();            
+        loop_rate.sleep();  // Use sleep in order to run this loop at 10Hz
     }
 
-    cv::destroyWindow("USB CAM image");
+    cv::destroyWindow("USB CAM image"); // Destroy the image window
 
     return 0;
 }
