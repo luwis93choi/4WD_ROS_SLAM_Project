287e38a104c7b8cdbb69d66f1146327ad335a90b
luwis93choi <luwis93choi@hotmail.com>
2019-11-10 17:01:39 +0900

    Corrected lidar scan data index error and Added extra comments for usb_cam_node reference

--- 20191029_ROS_RPLidar_Practice/src/process_node.cpp
+++ 20191029_ROS_RPLidar_Practice/src/process_node.cpp
@@ -1,3 +1,22 @@
+/*
+ * RPLidar HW datasheet/reference : http://bucket.download.slamtec.com/b42b54878a603e13c76a0a0500b53595846614c6/LR001_SLAMTEC_rplidar_protocol_v1.1_en.pdf 
+ *                                  http://www.slamtec.com/en/Lidar/A1
+ * 
+ * RPLidar SW reference : http://wiki.ros.org/rplidar 
+ *                        https://github.com/Slamtec/rplidar_ros 
+ *                        https://github.com/robopeak/rplidar_ros 
+ * 
+ * Applying RPLiar for SLAM : http://www.geduino.org/site/archives/35
+ *                            https://www.seeedstudio.com/blog/2018/11/09/rplidar-and-ros-the-best-way-to-learn-robot-and-slam/  
+ *                            https://answers.ros.org/question/205594/localization-problem-with-gmapping-and-rplidar/ 
+ *                            https://answers.ros.org/question/206185/why-rplidar-gmapping-has-a-bad-result/ 
+ * 
+ * RPLidar Orientation-related issues/references : https://github.com/robopeak/rplidar_ros/issues/77 
+ *                                                 https://github.com/robopeak/rplidar_ros/blob/master/rplidar_A1.png 
+ * 
+ * RPLidar Installation : "sudo apt-get install ros-<ros-distro>-rplidar-ros" (ex : sudo apt-get install ros-kinetic-rplidar-ros (for ROS Kinetic))
+ */
+
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 
@@ -9,7 +28,7 @@ void rplidar_RX_Callback(const sensor_msgs::LaserScan::ConstPtr& scan){
 
     ROS_INFO("Current Scan Time : [%f] / Current Sample Frequency : [%f]", scan->scan_time, 1/scan->scan_time);
     ROS_INFO("Current angle_min : %f / Current angle_increment : %f", scan->angle_min, scan->angle_increment);
-    for (int i = 0; i < count/2; i++){
+    for (int i = 0; i < count; i++){
 
         float degree = RAD2DEG(scan->angle_min + (scan->angle_increment * i));
         ROS_INFO("[%f : %f m]", degree, scan->ranges[i]);
