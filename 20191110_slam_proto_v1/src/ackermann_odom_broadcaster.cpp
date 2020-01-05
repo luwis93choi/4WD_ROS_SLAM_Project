2c580507090a791ca2a6f9bca341bf59bbd7426f
luwis93choi <luwis93choi@hotmail.com>
2020-01-06 00:28:24 +0900

    Adding Intel Realsense CAM + IMU into launch file & Adjustments on steering angle calculation for odometry

--- 20191110_slam_proto_v1/src/ackermann_odom_broadcaster.cpp
+++ 20191110_slam_proto_v1/src/ackermann_odom_broadcaster.cpp
@@ -2,6 +2,7 @@
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 #include <std_msgs/Int32.h>
+#include <sensor_msgs/Imu.h>
 
 // Odometry Variables //////////////////////////////////////////////
 double x = 0;               // [m]
@@ -29,6 +30,14 @@ double vel_delta_time = 0.0;    // [sec]
 int log_flag = 0;
 
 bool calc_flag = false;
+////////////////////////////////////////////////////////////////////////////////
+
+
+// Steering Angle Calculation Variables //////////////////////////////////////////////
+double gravitional_accel = 9.728;   // [m/s^2]
+double IMU_inclination_angle = 0;   // [radian]
+
+double tilt_angle_on_gravity_axis = 0;
 ////////////////////////////////////////////////////////////////////////////////
 
 void wheel_velocity_calcCB(const std_msgs::Int32& msg){
@@ -75,12 +84,48 @@ void velocity_ctrlCB(const std_msgs::Int32& msg){
     }
 }
 
-void steering_angle_calcCB(const std_msgs::Int32& msg){
+void steering_angle_calcCB(const sensor_msgs::Imu msg){
 
-    // steering_angle calculation based on servo PWM value
-    /*
-    steering_angle = msg.data * conversion_value;
-     */
+    // steering_angle calculation based on Realsense IMU
+    double quat_x = msg.orientation.x;
+    double quat_y = msg.orientation.y;
+    double quat_z = msg.orientation.z;
+    double quat_w = msg.orientation.w;
+
+    ROS_INFO("Quaternion orientation.x : %f", quat_x);
+    ROS_INFO("Quaternion orientation.y : %f", quat_y);
+    ROS_INFO("Quaternion orientation.z : %f", quat_z);
+    ROS_INFO("Quaternion orientation.w : %f \n", quat_w);
+/*
+    double linear_accel_x = msg.linear_acceleration.x;
+    double linear_accel_y = msg.linear_acceleration.y;
+    double linear_accel_z = msg.linear_acceleration.z;
+
+    double incline_calc_input = sqrt( (pow(linear_accel_y, 2) + pow(linear_accel_z, 2)) / pow(gravitional_accel, 2) );
+
+    if(incline_calc_input > 1){
+
+        incline_calc_input = 1; 
+        IMU_inclination_angle = acos(incline_calc_input);
+    }
+    else{
+
+        IMU_inclination_angle = acos(incline_calc_input);
+    }
+
+    ROS_INFO("Current IMU inclination angle : %f", IMU_inclination_angle);
+
+    tilt_angle_on_gravity_axis = atan( sqrt( pow(linear_accel_y, 2) + pow(linear_accel_z, 2) ) / linear_accel_z );
+
+    ROS_INFO("Current Tilt Angle on Gravity Axis : %f", tilt_angle_on_gravity_axis);
+
+    double steering_angle_input = ( (-1 * linear_accel_y * linear_accel_z) - (linear_accel_z * ( linear_accel_y * sin(IMU_inclination_angle) - gravitional_accel * cos(IMU_inclination_angle) - linear_accel_x * cos(IMU_inclination_angle) )) / 
+                                    linear_accel_y * (linear_accel_y * sin(IMU_inclination_angle) - gravitional_accel * cos(IMU_inclination_angle) - linear_accel_x * cos(IMU_inclination_angle)) - linear_accel_z * linear_accel_z);
+
+    steering_angle = atan(steering_angle_input);
+
+    ROS_INFO("Current steering_angle : %f \n", steering_angle);
+*/
 }
 
 int main(int argc, char** argv){
@@ -88,30 +133,40 @@ int main(int argc, char** argv){
     ros::init(argc, argv, "ackermann_odom");
     ros::NodeHandle nh;
 
+    // *******************************************************************************//
+    // Odometry TF Publisher 
     ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>("odom", 50);
     tf::TransformBroadcaster odom_broadcaster;
 
+    nh.setParam("body_length", body_length);
+    // *******************************************************************************//
+
+    // *******************************************************************************//
+    // Wheel Velocity Calculation
     ros::Subscriber wheel_velocity_calc = nh.subscribe("encoder", 1000, wheel_velocity_calcCB);
-    //ros::Subscriber steering_angle_calc = nh.subscribe("servo_ctrl", 10, steering_angle_calcCB);
-
-    ros::Subscriber velocity_dir_command = nh.subscribe("dc_direction_ctrl", 100, velocity_ctrlCB);
-    ros::Subscriber velocity_speed_command = nh.subscribe("dc_speed_ctrl", 100, velocity_ctrlCB);
-
+    
     ros::Time current_time, last_time;
     current_time = ros::Time::now();
     last_time = ros::Time::now();
 
-    nh.setParam("body_length", body_length);
+    vel_current_time = ros::Time::now();
+    vel_target_time = ros::Time::now();
+
+    ros::Subscriber velocity_dir_command = nh.subscribe("dc_direction_ctrl", 100, velocity_ctrlCB); // Used to detect whether the vehicle has stopped
+    ros::Subscriber velocity_speed_command = nh.subscribe("dc_speed_ctrl", 100, velocity_ctrlCB);   // Used to detect whether the vehicle has stopped
+    // *******************************************************************************//
+    
+    // *******************************************************************************//
+    // Steering Angle Calculation
+    ros::Subscriber steering_angle_calc = nh.subscribe("/imu/data", 100, steering_angle_calcCB);
+
+    // *******************************************************************************//
 
     ros::Rate loop_rate(100);   // This node will run at 100Hz
     // In order to conduct high-speed calculation for Odometry, we have to set loop rate at high Hz
 
 
     int test = 0;
-
-    vel_current_time = ros::Time::now();
-    vel_target_time = ros::Time::now();
-
 
     while(nh.ok()){
 
@@ -132,7 +187,7 @@ int main(int argc, char** argv){
             // *******************************************************************************//
             // Ackermann Steering Odometry Model
             theta = theta + ((wheel_velocity/body_length) * tan(steering_angle));
-            x = x + (wheel_velocity * cos(theta) * delta_time);
+            x =  x + (wheel_velocity * cos(theta) * delta_time);
             y = y + (wheel_velocity * sin(theta) * delta_time);
 
             // *******************************************************************************//
