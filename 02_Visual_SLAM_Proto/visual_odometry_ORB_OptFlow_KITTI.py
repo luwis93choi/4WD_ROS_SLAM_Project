5c6d7a8037b303e104ea6aac55443641eea2ddf8
luwis93choi <luwis93choi@hotmail.com>
2020-10-17 10:59:12 +0900

    Added frame skip / Compute 3D point cloud only when features are depleted / Added update function

--- 02_Visual_SLAM_Proto/visual_odometry_ORB_OptFlow_KITTI.py
+++ 02_Visual_SLAM_Proto/visual_odometry_ORB_OptFlow_KITTI.py
@@ -58,7 +58,9 @@ class mono_VO_ORBFlow_KITTI:
                                criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
         self.init_optical = True
+        self.init_cloud = True
         self.initial_common_match_num = 0
+        self.retracking_ratio = 0.5
 
         self.min_matches_num = 8
 
@@ -94,7 +96,10 @@ class mono_VO_ORBFlow_KITTI:
         self.dataset_end_idx = len(os.listdir(_dataset_path)) - 1
         self.dataset_current_idx = 0
 
-        # Prepare groundtruth list
+        self.geometric_unit_changes = {'R_pprev_prev' : None, 'T_pprev_prev' : None, 
+                                       'R_prev_current' : None, 'T_prev_current' : None}
+
+        ### Prepare groundtruth list #####################################################################
         self.ground_truth_T = []
         f = open(_dataset_pose_path, 'r')
         init_flag = 0
@@ -129,9 +134,22 @@ class mono_VO_ORBFlow_KITTI:
                                              [float(pose[8]), float(pose[9]), float(pose[10])]])
             
         f.close()
-        
+        #####################################################################################################
+
         print('Dataset Path : ', self.dataset_path)
         print('Ground Truth Path : ', _dataset_pose_path)
+
+    def frame_Skip(self):
+
+        pprev_match_keypoints_pts = np.float32(self.img_features_buffer[0])
+        prev_match_keypoints_pts = np.float32(self.img_features_buffer[1])
+        current_match_keypoints_pts = np.float32(self.img_features_buffer[2])
+
+        pixel_diff = np.mean(abs(current_match_keypoints_pts - prev_match_keypoints_pts))
+
+        print('[Pixel DIFF] : ',  pixel_diff)
+
+        return pixel_diff < 3
 
     def img_buffer_feature_tracking(self, disp_img=True):
 
@@ -228,10 +246,9 @@ class mono_VO_ORBFlow_KITTI:
             print('pprev common num : ', len(self.img_features_buffer[0]))
             print('prev common num : ', len(self.img_features_buffer[1]))
             print('current common num : ', len(self.img_features_buffer[2]))
-            print('--------------------------------')
 
-            ### Feature Retracking over all 3 images ###
-            if len(self.img_features_buffer[2]) < (self.initial_common_match_num * 0.25):
+            ### Feature Retracking over all 3 images ###################################################################################
+            if len(self.img_features_buffer[2]) < (self.initial_common_match_num * self.retracking_ratio):
                 print('[Re-Tracking] Not Enough Features between pprev and prev / Too many have been consumed')
 
                 ### pprev-prev ###
@@ -254,30 +271,21 @@ class mono_VO_ORBFlow_KITTI:
                 self.img_features_buffer[0] = (self.img_features_buffer[0][st==1]).reshape(-1, 1, 2)
                 self.img_features_buffer[1] = (self.img_features_buffer[1][st==1]).reshape(-1, 1, 2)
 
-                ### prev-current ###
-                # pprev Grayscale ORB feature extraction
-                prev_keypoints = self.orb.detect(self.image_buffer[1], None)
-                prev_keypoints, pprev_descriptors = self.orb.compute(self.image_buffer[1], prev_keypoints)
-                prev_keypoints = np.array((prev_keypoints))
-
-                # pprev Datatype converison for optical flow
-                temp_prev_keypoints = []
-                for keypoint in prev_keypoints:
-                    temp_prev_keypoints.append([[np.float32(keypoint.pt[0]), np.float32(keypoint.pt[1])]])
-
-                self.img_features_buffer[1] = np.array(temp_prev_keypoints)
-
-                # Re-Track between pprev and prev
+                # Re-Track between prev and current
                 self.img_features_buffer[2], st, err = cv.calcOpticalFlowPyrLK(self.image_buffer[1], self.image_buffer[2], 
                                                                                self.img_features_buffer[1], None, **self.lk_params)
 
                 self.img_features_buffer[1] = (self.img_features_buffer[1][st==1]).reshape(-1, 1, 2)
                 self.img_features_buffer[2] = (self.img_features_buffer[2][st==1]).reshape(-1, 1, 2)
 
+                self.img_features_buffer[0] = (self.img_features_buffer[0][st==1]).reshape(-1, 1, 2)
+
                 # Update common cloud number
                 self.initial_common_match_num = len(self.img_features_buffer[2])
+            
+                self.init_cloud = True
 
-            ############################################
+            ##################################################################################################################################
 
             # Display Optical Flow results if the display option is True
             if disp_img == True:
@@ -300,12 +308,160 @@ class mono_VO_ORBFlow_KITTI:
                 cv.imshow('ORB-based Optical Flow + ReTracking', img)
                 k = cv.waitKey(30)
 
-            # Update current values as prev values
-            self.image_buffer[0] = self.image_buffer[1]
-            self.img_features_buffer[0] = self.img_features_buffer[1]
-
-            self.image_buffer[1] = self.image_buffer[2]
-            self.img_features_buffer[1] = self.img_features_buffer[2]
-
             self.dataset_current_idx += 1
 
+    def geometric_change_calc(self):
+        
+        pprev_match_keypoints_pts = self.img_features_buffer[0]
+
+        prev_match_keypoints_pts = self.img_features_buffer[1]
+
+        current_match_keypoints_pts = self.img_features_buffer[2]
+
+        ### Essential Matrix Calcuation & Rotation/Translation Matrix Calculation ###
+        Essential_Mat_pprev_prev, mask_pprev_prev = cv.findEssentialMat(np.int32(prev_match_keypoints_pts), 
+                                                                        np.int32(pprev_match_keypoints_pts),
+                                                                        cameraMatrix=self.intrinsic_CAM_Mat,
+                                                                        method=cv.RANSAC, prob=0.999, threshold=1.0)
+
+        Essential_Mat_prev_current, mask_prev_current = cv.findEssentialMat(np.int32(current_match_keypoints_pts), 
+                                                                            np.int32(prev_match_keypoints_pts),
+                                                                            cameraMatrix=self.intrinsic_CAM_Mat,
+                                                                            method=cv.RANSAC, prob=0.999, threshold=1.0)
+
+        retval, Rotation_Mat_pprev_prev, Translation_Mat_pprev_prev, r_mask_pprev_prev = cv.recoverPose(Essential_Mat_pprev_prev,
+                                                                                                        np.int32(prev_match_keypoints_pts),
+                                                                                                        np.int32(pprev_match_keypoints_pts),
+                                                                                                        cameraMatrix=self.intrinsic_CAM_Mat)
+
+        retval, Rotation_Mat_prev_current, Translation_Mat_prev_current, r_mask_prev_current = cv.recoverPose(Essential_Mat_prev_current,
+                                                                                                              np.int32(current_match_keypoints_pts),
+                                                                                                              np.int32(prev_match_keypoints_pts),
+                                                                                                              cameraMatrix=self.intrinsic_CAM_Mat)
+
+        self.geometric_unit_changes['R_pprev_prev'] = Rotation_Mat_pprev_prev
+        self.geometric_unit_changes['T_pprev_prev'] = Translation_Mat_pprev_prev
+        self.geometric_unit_changes['R_prev_current'] = Rotation_Mat_prev_current
+        self.geometric_unit_changes['T_prev_current'] = Translation_Mat_prev_current
+
+        print('[R_pprev_prev]')
+        print(Rotation_Mat_pprev_prev)
+        print('[T_pprev_prev]')
+        print(Translation_Mat_pprev_prev)
+        print('[R_prev_current]')
+        print(Rotation_Mat_prev_current)
+        print('[T_prev_current]')
+        print(Translation_Mat_prev_current)
+
+        return self.geometric_unit_changes
+
+    def img_common3Dcloud_triangulate(self):
+        
+        pprev_match_keypoints_pts = np.float32(self.img_features_buffer[0]).reshape(2, -1)
+        prev_match_keypoints_pts = np.float32(self.img_features_buffer[1]).reshape(2, -1)
+        current_match_keypoints_pts = np.float32(self.img_features_buffer[2]).reshape(2, -1)
+
+        Rotation_Mat_pprev_prev = self.geometric_unit_changes['R_pprev_prev']
+        Translation_Mat_pprev_prev = self.geometric_unit_changes['T_pprev_prev']
+        Rotation_Mat_prev_current = self.geometric_unit_changes['R_prev_current']
+        Translation_Mat_prev_current = self.geometric_unit_changes['T_prev_current']
+
+        if self.init_cloud == True:
+            print('[INFO] pprev-prev Triangulation')
+            ### Triangluation between pprev and prev
+            # The canonical matrix (set as the origin)
+            P0 = np.array([[1, 0, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 1, 0]])
+            P0 = self.intrinsic_CAM_Mat.dot(P0)
+            # Rotated and translated using P0 as the reference point
+            P1 = np.hstack((Rotation_Mat_pprev_prev, Translation_Mat_pprev_prev))
+            P1 = self.intrinsic_CAM_Mat.dot(P1)
+
+            self.pprev_prev_cloud = cv.triangulatePoints(P0, P1, pprev_match_keypoints_pts, prev_match_keypoints_pts).reshape(-1, 4)[:, :3]
+
+            self.init_cloud = False
+
+        print('[INFO] prev-current Triangulation')
+        ### Triangluation between prev and current
+        # The canonical matrix (set as the origin)
+        P0 = np.array([[1, 0, 0, 0],
+                       [0, 1, 0, 0],
+                       [0, 0, 1, 0]])
+        P0 = self.intrinsic_CAM_Mat.dot(P0)
+        # Rotated and translated using P0 as the reference point
+        P1 = np.hstack((Rotation_Mat_prev_current, Translation_Mat_prev_current))
+        P1 = self.intrinsic_CAM_Mat.dot(P1)
+
+        self.prev_current_cloud = cv.triangulatePoints(P0, P1, prev_match_keypoints_pts, current_match_keypoints_pts).reshape(-1, 4)[:, :3]
+
+        
+        # Remove the triangulation results with negative Z value 
+        # (Remove the triangulation values that are projected behind image plane)
+        #corrected_pprev_prev_cloud = []
+        #corrected_prev_current_cloud = []
+        #for i in range(len(pprev_prev_cloud)):
+        #    if (pprev_prev_cloud[i][2] >= 0) and (prev_current_cloud[i][2] >= 0):
+        #        corrected_pprev_prev_cloud.append(pprev_prev_cloud[i])
+        #        corrected_prev_current_cloud.append(prev_current_cloud[i])
+        #
+        #return corrected_pprev_prev_cloud, corrected_prev_current_cloud
+        
+        return self.pprev_prev_cloud, self.prev_current_cloud
+
+    def pose_estimate(self):
+
+        print('pprev-prev cloud num : ', len(self.pprev_prev_cloud))
+        print('prev-current cloud num : ', len(self.prev_current_cloud))
+
+        ratios = []
+        for i in range(len(self.prev_current_cloud)):
+            if i > 0:
+                current_Xk1 = self.prev_current_cloud[i]
+                current_Xk2 = self.prev_current_cloud[i-1]
+
+                prev_Xk1 = self.pprev_prev_cloud[i]
+                prev_Xk2 = self.pprev_prev_cloud[i-1]
+
+                if (np.linalg.norm(current_Xk1 - current_Xk2) != 0):
+                    ratios.append(np.linalg.norm(prev_Xk1 - prev_Xk2) / np.linalg.norm(current_Xk1 - current_Xk2))
+
+        T_relative_scale = np.median(ratios)
+        #T_relative_scale = np.mean(ratios)
+
+        print('Relative Scale for Translation : ', T_relative_scale)
+
+        absolute_scale = np.linalg.norm(np.array(self.ground_truth_T[self.dataset_current_idx]) - np.array(self.ground_truth_T[self.dataset_current_idx-1]))
+        print('Absolute Sclae for Translation : ', absolute_scale)
+        
+        # Apply Forward Dominant Motoin Model with Absolute Value Comparison. 
+        # This is implemented to prevent the error of accumulating Translation and Rotation when the camera is stationary, but the image is changing.
+        # This condition is used to prevent nearby moving object's feature keypoint changes from affecting the calculation of translation and rotation.
+        # Absolute value of Z is compared to absolute value of Y and X. Absolute value is used to make this condition work under both foward and backward movements of the camera.
+        if ( (abs(self.geometric_unit_changes['T_prev_current'][2]) > abs(self.geometric_unit_changes['T_prev_current'][0])) and
+             (abs(self.geometric_unit_changes['T_prev_current'][2]) > abs(self.geometric_unit_changes['T_prev_current'][1])) ):
+            self.pose_T = self.prev_pose_T + T_relative_scale * self.prev_pose_R.dot(self.geometric_unit_changes['T_prev_current'])
+            self.pose_R = self.geometric_unit_changes['R_prev_current'].dot(self.prev_pose_R)
+
+            print('[INFO] Dominant Forward : Pose Estimation Results')
+            print(self.pose_T)
+            print(self.pose_R)
+
+            return self.pose_T, self.pose_R
+
+    def update(self):
+
+        # Update current values as prev values
+        self.image_buffer[0] = self.image_buffer[1]
+        self.img_features_buffer[0] = self.img_features_buffer[1]
+
+        self.image_buffer[1] = self.image_buffer[2]
+        self.img_features_buffer[1] = self.img_features_buffer[2]
+
+        self.geometric_unit_changes['R_pprev_prev'] = self.geometric_unit_changes['R_prev_current']
+        self.geometric_unit_changes['T_pprev_prev'] = self.geometric_unit_changes['T_prev_current']
+
+        self.pprev_prev_cloud = self.prev_current_cloud
+
+        self.prev_pose_T = self.pose_T
+        self.prev_pose_R = self.pose_R
